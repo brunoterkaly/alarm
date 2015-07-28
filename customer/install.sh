c1f9cf2491eb04d2e78831e97fc96307e0323c69
unknown <bterkaly@BRUNOLENOVO.redmond.corp.microsoft.com>
2015-07-28 11:21:05 -0700

    updates

---
+++ customer/install.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+# Custom BYOL install script for Ubuntu 14.02
+# $1 = licenseKey
+
+logger "Installing Azure Dev Environment"
+
+customer=$1
+
+echo $customer >> /tmp/log.txt
+
+sudo apt-get install --yes nodejs-legacy
+sudo apt-get install --yes npm
+sudo npm install -g azure-cli
+
+# install git
