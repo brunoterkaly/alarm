1f023c4fe23351b376bfd39ad9cd8b95b86b6a42
Jim Zimmerman <jim@jimzimmerman.com>
2015-07-29 00:57:29 -0700

    added powershell script to deploy linux vm

---
+++ devenvironment/deploy-linux-dev.ps1
@@ -0,0 +1,22 @@
+#Requires -Version 3.0
+
+Param(
+  [string] $ResourceGroupName = 'linux-azure-cli',
+  [string] $ResourceGroupLocation = "West US",
+  [string] $TemplateFile = '.\azuredeploy.json',
+  [string] $ParametersFile = '.\azuredeploy-parameters.json',
+)
+
+Set-StrictMode -Version 3
+
+# Convert relative paths to absolute paths if needed
+$TemplateFile = [System.IO.Path]::Combine($PSScriptRoot, $TemplateFile)
+$ParametersFile = [System.IO.Path]::Combine($PSScriptRoot, $ParametersFile)
+
+# Create or update the resource group using the specified template file and template parameters file
+Switch-AzureMode AzureResourceManager
+New-AzureResourceGroup -Name $ResourceGroupName `
+                       -Location $ResourceGroupLocation `
+                       -TemplateFile $TemplateFile `
+                       -TemplateParameterFile $ParametersFile `
+                       -Force -Verbose
