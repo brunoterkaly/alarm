91ef3b5724f5e31c266f09b00145a057edc5183e
Jim Zimmerman <jim@jimzimmerman.com>
2015-07-29 11:48:44 -0700

    changed name of single vm and added vm id to the output

--- customerdata-singlevm/install.sh
+++ customerdata-singlevm/install.sh
@@ -2,14 +2,14 @@
 # Custom BYOL install script for Ubuntu 14.02
 # $1 = licenseKey
 
-logger "Installing Azure Dev Environment"
+logger "Installing Customer Data Template"
 
 customerName=$1
 contactName=$2
 contactEmail=$3
 customerAzureSubscriptionId=$4
 
-
+sudo dmidecode | grep UUID >> /tmp/log.txt
 echo $customerName >> /tmp/log.txt
 echo $contactName >> /tmp/log.txt
 echo $contactEmail >> /tmp/log.txt
